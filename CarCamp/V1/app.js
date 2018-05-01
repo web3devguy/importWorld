67ccc729aa4a0b954a0203d3fbcea16bbfd481d7
insideoutzombie <david@web3devs.com>
2018-05-01 17:26:54 -0500

    add much more content to cargrounds.ejs and header.ejs

--- CarCamp/V1/app.js
+++ CarCamp/V1/app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 
 var cargrounds = [
     {name: "Nissan Skyline R34", image: "https://www.rightdrive.ca/wp-content/uploads/2015/10/nissan-skyline-gtr-r34-1.jpg"},
-    {name: "HONDA Heaven", image: "https://mklr.pl/uimages/services/motokiller/i18n/pl_PL/201606/$_honda_b16_b18_h22_k20_k24_dohc_vtec_$1465398125_by_Tech.jpg?1465398125"},
+    {name: "Honda Heaven", image: "https://mklr.pl/uimages/services/motokiller/i18n/pl_PL/201606/$_honda_b16_b18_h22_k20_k24_dohc_vtec_$1465398125_by_Tech.jpg?1465398125"},
     {name: "Import Allaince", image: "https://i2.wp.com/importalliance.org/wp-content/uploads/2017/03/IMG_6243.jpg?ssl=1"},
     {name: "Civic Type-R FK8", image: "https://www.technobuffalo.com/wp-content/uploads/2017/06/hondacivictyper14-470x310@2x.jpg"}
 ];
@@ -23,7 +23,7 @@ app.get("/cargrounds", function(req, res){
 app.post("/cargrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var newCarground = {name: name, image: image}
     cargrounds.push(newCarground);
     res.redirect("/cargrounds");
 });
