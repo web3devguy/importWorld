b1f59b80a885f5dd6b424bef421c629866ca4fed
insideoutzombie <david@web3devs.com>
2018-05-02 17:25:42 -0500

    much more additons to the code

--- CarCamp/V1/app.js
+++ CarCamp/V1/app.js
@@ -1,31 +1,69 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express      = require("express"),
+    app          = express(),
+    bodyParser   = require("body-parser"),
+    mongoose     = require("mongoose");
 
+mongoose.connect("mongodb://localhost/car_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var cargrounds = [
-    {name: "Nissan Skyline R34", image: "https://www.rightdrive.ca/wp-content/uploads/2015/10/nissan-skyline-gtr-r34-1.jpg"},
-    {name: "Honda Heaven", image: "https://mklr.pl/uimages/services/motokiller/i18n/pl_PL/201606/$_honda_b16_b18_h22_k20_k24_dohc_vtec_$1465398125_by_Tech.jpg?1465398125"},
-    {name: "Import Allaince", image: "https://i2.wp.com/importalliance.org/wp-content/uploads/2017/03/IMG_6243.jpg?ssl=1"},
-    {name: "Civic Type-R FK8", image: "https://www.technobuffalo.com/wp-content/uploads/2017/06/hondacivictyper14-470x310@2x.jpg"}
-];
+// SCHEMA SETUP
+var cargroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Carground = mongoose.model("Carground", cargroundSchema);
+
+// Carground.create(
+//     {
+//     name: "Nissan Skyline R34",
+//     image: "https://www.rightdrive.ca/wp-content/uploads/2015/10/nissan-skyline-gtr-r34-1.jpg"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED CARGROUND: ");
+//             console.log(cargrounds);
+//         }
+//     });
+// //
+// //
+// var cargrounds = [
+// //     {name: "Nissan Skyline R34", image: "https://www.rightdrive.ca/wp-content/uploads/2015/10/nissan-skyline-gtr-r34-1.jpg"},
+// //     {name: "Honda Heaven", image: "https://mklr.pl/uimages/services/motokiller/i18n/pl_PL/201606/$_honda_b16_b18_h22_k20_k24_dohc_vtec_$1465398125_by_Tech.jpg?1465398125"},
+// //     {name: "Import Allaince", image: "https://i2.wp.com/importalliance.org/wp-content/uploads/2017/03/IMG_6243.jpg?ssl=1"},
+//     {name: "Civic Type-R FK8", image: "https://www.technobuffalo.com/wp-content/uploads/2017/06/hondacivictyper14-470x310@2x.jpg"}
+// ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/cargrounds", function(req, res){
-    res.render("cargrounds",{cargrounds:cargrounds});
+    // Get all cargrounds from DB
+    Carground.find({}, function(err, allCargrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("cargrounds",{cargrounds:allCargrounds});
+        }
+    });
 });
 
 app.post("/cargrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCarground = {name: name, image: image}
-    cargrounds.push(newCarground);
-    res.redirect("/cargrounds");
+    var newCarground = {name: name, image: image};
+    // Create a new carground and save to DB
+    Carground.create(newCarground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+        // redirect back to cargrounds page
+            res.redirect("/cargrounds");
+        }
+    });
 });
 
 app.get("/cargrounds/new", function(req, res) {
